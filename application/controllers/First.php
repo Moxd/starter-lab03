347c2f2050009b7f1613653ae53c61bf6d9d0f98
amorishi <a.kiyohito.m@hotmail.com>
2015-09-24 14:53:59 -0700

    Fixed a bug in First.php

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -24,11 +24,9 @@ class First extends Application {
     function index() {
         $this->data['pagebody'] = 'justone';    // this is the view we want shown
         // build the list of authors, to pass on to our view
-        $this->data = array_merge($this->data, $source);
+        $source = $this->quotes->first();
     
-        $this->data['who'] = $source['who'];
-        $this->data['mug'] = $source['mug'];
-        $this->data['what'] = $source['what'];
+        $this->data = array_merge($this->data, $source);
         
         $this->render();
     }
